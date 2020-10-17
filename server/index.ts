6e8a0e6978c2bc551c7b5ce433d1db3f3595ea22
Nathan Knowler <nathan@knowler.me>
2020-10-17 02:21:11 -0500

    Begin re-adding Apollo

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,50 @@
 import {createServer} from 'http';
 import {readFile} from 'fs';
 import path from 'path';
+import mime from 'mime-types';
+import {ApolloServer, gql} from 'apollo-server';
 
-import {html} from './html'
+import {html} from './html';
+
+const posts = [
+  {
+    id: 0,
+    title: 'Hello, World!',
+    author: 'Nathan Knowler',
+  },
+];
+
+const apolloServer = new ApolloServer({
+  typeDefs: gql`
+    type Post {
+      id: ID!
+      title: String
+      author: String
+    }
+
+    type Query {
+      posts: [Post]
+    }
+
+    type Mutation {
+      createPost(title: String, author: String): Post
+    }
+  `,
+  resolvers: {
+    Query: {
+      posts: () => posts,
+    },
+    Mutation: {
+      createPost: (_, post) => {
+        const id = posts.length;
+
+        posts.push({id, ...post});
+
+        return posts[id];
+      },
+    },
+  },
+});
 
 const server = createServer((req, res) => {
   switch (req.url) {
@@ -13,6 +55,14 @@ const server = createServer((req, res) => {
       break;
     }
     default: {
+      const contentType = mime.lookup(req.url);
+
+      if (!contentType) {
+        console.error('Bad mime type');
+        res.writeHead(415);
+        res.end();
+      }
+
       readFile(path.resolve(__dirname, '..', 'client', req.url.replace(/^\//, '')), (err, data) => {
         if (err) {
           console.error(err);
@@ -21,7 +71,7 @@ const server = createServer((req, res) => {
         }
 
         res.writeHead(200, {
-          'Content-Type': 'application/javascript',
+          'Content-Type': contentType as string,
           'X-Content-Type-Options': 'nosniff',
         });
         res.write(data);
@@ -33,4 +83,10 @@ const server = createServer((req, res) => {
   }
 });
 
-server.listen(8080);
+server.listen(8080, () => {
+  console.log(`Server running at http://localhost:8080/`);
+});
+
+apolloServer.listen().then(({url}) => {
+  console.log(`Apollo server running at ${url}`);
+});
